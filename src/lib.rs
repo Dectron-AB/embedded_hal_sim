3027a521c16fabe32686a74dd95f916c85c99665
Albin Hedman <albin9604@gmail.com>
2026-01-01 20:45:15 +0100

    Remove leftovers

--- src/lib.rs
+++ src/lib.rs
@@ -4,18 +4,3 @@ pub mod ethernet;
 #[cfg(feature = "flash")]
 pub mod flash;
 pub mod gpio;
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
