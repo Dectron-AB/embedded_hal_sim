4a3fd53257a914b10c686ddd154a26f3f4fc8bfe
Albin Hedman <albin9604@gmail.com>
2026-01-06 01:00:30 +0100

    Cleanup debug info

--- src/ethernet.rs
+++ src/ethernet.rs
@@ -29,27 +29,33 @@ pub fn new<'d, const MTU: usize, const N_RX: usize, const N_TX: usize>(
 impl<'d, const MTU: usize> EthernetBackgroundRunner<'d, MTU> {
     /// This needs to be run from a tokio executor
     pub async fn run(self) -> ! {
-        let (_s, mut rx_buff, mut tx_buff) = self.runner.split();
+        let (state, mut rx_buff, mut tx_buff) = self.runner.split();
+        state.set_link_state(driver::LinkState::Up);
+
         let iface = self.iface;
 
-        log::trace!("Boop");
-
+        let mut buf = [0u8; MTU];
+        let mut bytes_available = 0;
+        
         loop {
-            log::trace!("Baap");
             // Wait for either..
             tokio::select! {
-                n = async {
-                    // the buffer is ready...
-                    let buf = rx_buff.rx_buf().await;
-                    // ... *AND* the interface is signaling an interrupt, indicating a packet is available to receive
-                    iface.recv(buf).await.unwrap()
+                rx_buf = async {
+                    // the interface is signaling an interrupt, indicating a packet is available to receive
+                    if bytes_available == 0 {
+                        bytes_available = iface.recv(&mut buf).await.unwrap();
+                    }
+                    // ... *AND* the buffer is ready...
+                    rx_buff.rx_buf().await
                 } => {
-                    log::trace!("Poop");
-                    rx_buff.rx_done(n)
+                    log::trace!("rx: {bytes_available}");
+                    rx_buf[..bytes_available].copy_from_slice(&buf[..bytes_available]);
+                    rx_buff.rx_done(bytes_available);
+                    bytes_available = 0;
                 }
                 // ... or a TX buffer becoming available, i.e. embassy-net wants to send a packet
                 buf = tx_buff.tx_buf() => {
-                    log::trace!("Paap");
+                    log::trace!("tx: {}", buf.len());
                     // a packet is ready to be sent!
                     iface.send_all(buf).await.unwrap();
                     tx_buff.tx_done();
